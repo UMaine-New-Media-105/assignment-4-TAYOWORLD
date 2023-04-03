b85d7d4c24091d9463166338f76e7efb73e35245
TAYOWORLD <124199388+TAYOWORLD@users.noreply.github.com>
2023-04-03 13:02:12 -0400

    Create First_Part.js

---
+++ First_Part.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+}
+
+function draw() {
+  background("grey");
+   
+funnyStar(50,50,10)
+  
+funnyStar(200,80,15)
+  
+funnyStar(120,200,8)
+  
+}
+function funnyStar(x, y, size) {
+  push(); 
+  translate(x, y); 
+  scale(size);
+  beginShape();
+  for (let i = 0; i < 5; i++) {
+    let angle = TWO_PI * i / 5 - HALF_PI;
+    let x = cos(angle);
+    let y = sin(angle);
+    vertex(x, y);
+    angle += TWO_PI * 2 / 5;
+    x = cos(angle) * 0.5;
+    y = sin(angle) * 0.5;
+    vertex(x, y);
+  }
+  endShape(CLOSE);
+  pop(); 
+}
